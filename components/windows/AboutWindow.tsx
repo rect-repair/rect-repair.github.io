88c90fbf5945a0797db0199bfaff4e14d53a6173
jyol <joannaliu19@gmail.com>
2025-11-17 14:14:51 +0800

    correction

--- components/windows/AboutWindow.tsx
+++ components/windows/AboutWindow.tsx
@@ -7,7 +7,8 @@ export default function AboutWindow() {
     <div className='h-full bg-white text-black p-6'>
       <div className='space-y-4 text-sm leading-relaxed'>
         <p>
-                  [[rect*]]repair is a small art-game collective (mostly 3 people) focusing on making and facilitating experimental games, alternative game jams, community events and public interventions through self-organized events and collaborations with friends and sometimes institutions (ദ്ദി˙ ᴗ ˙ ). Usually happening in Shanghai/Asia.
+                  [[rect*]]repair is an art-game collective focusing on making and facilitating experimental games, alternative game jams, community events and public interventions through self-organized events and collaborations with friends and sometimes institutions (ദ്ദി˙ ᴗ ˙ ). Usually happening in Shanghai/Asia.
+
         </p>
         
       </div>
